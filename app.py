62bac23efae656b5a97dbe64deea80e4dd7b12c7
Ruzait <141403098+NexTechGen@users.noreply.github.com>
2024-02-29 10:33:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import sqlite3
+from flask import *
+
+app = Flask(__name__) 
+# creat DB
+'''conn = sqlite3.connect('Divisions.db')
+
+c = conn.cursor()
+
+c.execute("""CREATE TABLE customers(
+            first_name text,
+            last_name text,
+            house_num text
+            )""")
+
+
+conn.commit()
+
+conn.close()'''
+
+@app.route('/')   
+def main():   
+    return render_template("index.html")
+
+@app.route('/data', methods=["POST", "GET"])   
+def data():   
+    fname = request.form['fname']
+    lname = request.form['lname']
+    hnum = request.form['hnum']
+
+    conn = sqlite3.connect('Divisions.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO customers VALUES (?,?,?)", (fname, lname, hnum))
+
+    print("Command executed succesfully...")
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for("sub"))
+
+@app.route('/submit')   
+def sub(): 
+    conn = sqlite3.connect('Divisions.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM customers")   
+    data = c.fetchall() 
+    print(c.fetchall())
+
+    return render_template("dat.html", data = data)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
